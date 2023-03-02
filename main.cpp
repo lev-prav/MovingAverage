c69a3f3d469296cffd62505c037fb023820c8a0b
Lev Bratchenko <IamOnyfriy@gmail.com>
2023-03-02 14:11:55 +0300

    save signals

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <chrono>
 #include <iostream>
+#include <fstream>
 #include "moving_average.h"
 
 
@@ -35,17 +36,31 @@ long long test(int signal_size, int window_size, bool print = false){
 	//std::cout << "The time: " << elapsed_ms.count() << " ms\n";
 	
 	if (print) {
-		std::cout << "Random signal: \n";
-		for (T& num : random_signal) {
-			std::cout << num << " ";
-		}
-		std::cout << "\n\n";
+		auto type_name = typeid(random_signal[0]).name();
+		{
+			char file_name[120];
+			std::sprintf(file_name, "signals_random_%s.txt", type_name);
 
-		std::cout << "Smooth signal: \n";
-		for (T& num : smooth_signal) {
-			std::cout << num << " ";
+			std::ofstream fout(file_name);
+
+			//fout << "Random signal: \n";
+			for (T& num : random_signal) {
+				fout << num << " ; ";
+			}
+			//fout << "\n\n";
 		}
-		std::cout << "\n\n";
+		{
+			char file_name[120];
+			std::sprintf(file_name, "signals_smooth_%s.txt", type_name);
+
+			std::ofstream fout(file_name);
+
+			//fout << "Smooth signal: \n";
+			for (T& num : smooth_signal) {
+				fout << num << " ; ";
+			}
+			//fout << "\n\n";
+		}
 	}
 
 
@@ -56,16 +71,22 @@ long long test(int signal_size, int window_size, bool print = false){
 int main(int argc, char** argv) {
 	int signal_size = 1'000'000;
 	int windows[]{ 4, 8, 16, 32, 64, 128 };
-	std::cout << "Measure double: \n";
-	for (int window_size : windows) {
-		auto time = test<double>(signal_size, window_size, false);
-		std::cout << window_size << " : " << time << " mls \n";
-	}
+	//std::cout << "Measure double: \n";
+	//for (int window_size : windows) {
+	//	auto time = test<double>(signal_size, window_size);
+	//	std::cout << window_size << " : " << time << " mls \n";
+	//}
 
-	std::cout << "Measure float:\n";
-	for (int window_size : windows) {
-		auto time = test<float>(signal_size, window_size, false);
-		std::cout << window_size << " : " << time << " mls \n";
-	}
+	//std::cout << "Measure float:\n";
+	//for (int window_size : windows) {
+	//	auto time = test<float>(signal_size, window_size);
+	//	std::cout << window_size << " : " << time << " mls \n";
+	//}
+
+	auto time_d = test<double>(signal_size, windows[1], true);
+	std::cout << "Double done\n";
+	auto time_f = test<float>(signal_size, windows[1], true);
+	std::cout << "Float done\n";
+
 	return 0;
 }
