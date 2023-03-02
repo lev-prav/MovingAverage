bfc74b10b2e0b6464608d4d0c12147aff37e9190
Lev Bratchenko <IamOnyfriy@gmail.com>
2023-03-02 13:47:18 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 
 
 template<typename T>
-std::vector<T>&& generate_signal(int signal_size) {
+std::vector<T> generate_signal(int signal_size) {
 	std::random_device rd;
 	std::default_random_engine eng(rd());
-	std::uniform_real_distribution<T> distr(-10'000.f, 10'000.f);
+	std::uniform_real_distribution distr(-10'000.f, 10'000.f);
 
 	std::vector<T> signal;
 	signal.reserve(signal_size);
@@ -18,13 +18,13 @@ std::vector<T>&& generate_signal(int signal_size) {
 		signal.push_back(distr(eng));
 	}
 
-	return std::move(signal)
+	return signal;
 }
 
 template<typename T>
 long long test(int signal_size, int window_size, bool print = false){
 
-	std::vector<T> random_signal = generate_signal(signal_size);
+	std::vector<T> random_signal = generate_signal<T>(signal_size);
 
 	auto begin = std::chrono::steady_clock::now();
 
@@ -32,7 +32,7 @@ long long test(int signal_size, int window_size, bool print = false){
 
 	auto end = std::chrono::steady_clock::now();
 	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-	std::cout << "The time: " << elapsed_ms.count() << " ms\n";
+	//std::cout << "The time: " << elapsed_ms.count() << " ms\n";
 	
 	if (print) {
 		std::cout << "Random signal: \n";
@@ -54,8 +54,18 @@ long long test(int signal_size, int window_size, bool print = false){
 
 
 int main(int argc, char** argv) {
-	
-	auto time = test<double>(1024, 8);
+	int signal_size = 1'000'000;
+	int windows[]{ 4, 8, 16, 32, 64, 128 };
+	std::cout << "Measure double: \n";
+	for (int window_size : windows) {
+		auto time = test<double>(signal_size, window_size, false);
+		std::cout << window_size << " : " << time << " mls \n";
+	}
 
+	std::cout << "Measure float:\n";
+	for (int window_size : windows) {
+		auto time = test<float>(signal_size, window_size, false);
+		std::cout << window_size << " : " << time << " mls \n";
+	}
 	return 0;
 }
--- moving_average.h
+++ moving_average.h
@@ -7,13 +7,13 @@ using specializtion = std::enable_if_t <std::is_floating_point<T>::value>;
 
 
 template<typename T, typename = specializtion<T>>
-std::vector<T>&& moving_avarage(T* signal, int signal_length, int window_size) {
+std::vector<T> moving_avarage(const T* signal, int signal_length, int window_size) {
 
-	assert(0 < window_size && window_size <= data_size);
+	assert(0 < window_size && window_size <= signal_length);
 
 	T sum = 0, window_koef = T(window_size);
 	for (int i = 0; i < window_size; i++) {
-		sum += data[i];
+		sum += signal[i];
 	}
 
 	std::vector<T> smooth_data{sum/window_koef};
@@ -21,10 +21,10 @@ std::vector<T>&& moving_avarage(T* signal, int signal_length, int window_size) {
 
 	for (int i = 0, window_end = window_size; window_end < signal_length; window_end++, i++) {
 		int window_start = window_end - window_size;
-		T window_sum = sum - data[window_start] + data[window_end];
+		T window_sum = sum - signal[window_start] + signal[window_end];
 		smooth_data.push_back(window_sum / window_koef);
 		sum = window_sum; 
 	}
 
-	return std::move(smooth_data);
+	return smooth_data;
 }
